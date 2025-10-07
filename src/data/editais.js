045417cf9033060b3bc92d59ea2640df7e8af335
Luis Eduardo <luixst1@gmail.com>
2025-10-07 14:50:14 -0300

    /chore: Novo edital START

--- src/data/editais.js
+++ src/data/editais.js
@@ -2,7 +2,7 @@ export const editaisData = [
   {
     id: 1,
     status: "finalizado",
-    title: "Edital de Chamada Pública - Módulo I (Iniciante)",
+    title: "Edital de Chamada Pública Nº 02/2025 - Módulo I (Iniciante)",
     description:
       "Processo seletivo para o Curso de Introdução à Robótica (32h) - FINALIZADO.",
     details: [
@@ -27,5 +27,34 @@ export const editaisData = [
     ],
   },
 
-  // Adicione novos editais aqui.
+  {
+    id: 2,
+    status: "vigente",
+    title: "Edital de Chamada Pública Nº 03/2025 - Módulo I (Iniciante)",
+    description:
+      "Inscrições abertas para o Curso de Introdução à Robótica (32h)",
+    details: [
+      { label: "Inscrição", value: "6 a 15 de Outubro" },
+      { label: "Vagas", value: "80" },
+      { label: "Resultados", value: "16 de Outubro (1ª chamada)" },
+      { label: "Matrícula 1ª chamada", value: "16 a 20 de Outubro" },
+      { label: "Matrícula 2ª chamada", value: "21 a 24 de Outubro" },
+      { label: "Início das aulas", value: "20 de Outubro" },
+      { label: "Status", value: "Inscrições Abertas", isStatus: true },
+    ],
+    links: [
+      {
+        href: "https://forms.office.com/r/pNcmL55mh7",
+        text: "Fazer Inscrição",
+        primary: true,
+      },
+      {
+        href: "/pdf/CHAMADA-PUBLICA-No-02_2025_PROEX_IFG-SECTI-PROGRAMA-START-1.pdf",
+        text: "Acessar Edital (PDF)",
+        primary: false,
+      },
+    ],
+  },
+
+  //novos editais podem ser adicionados aqui
 ];
